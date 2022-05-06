4da680e067758ea631ac70ae19b277531f8d2e34
Hannes Preishuber <hannesp@ppedv.de>
2022-05-06 08:39:13 +0200

    HTML Formular Elemente am Beispiel

---
+++ BlazorTrainingMai22/Pages/Modul5/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BlazorTrainingMai22.Pages.Modul5
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ BlazorTrainingMai22/Pages/Modul5/Person.cs
@@ -0,0 +1,10 @@
+﻿namespace BlazorTrainingMai22.Pages.Modul5
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Gross { get; set; }
+        public DateTime GebDat { get; set; }
+    }
+}
