3ef78f410e6cf749bb1fe7949944ca3bf37059b1
Hannes Preishuber <hannesp@ppedv.de>
2022-05-06 09:44:53 +0200

    Boostrap isvalid css fix

---
+++ BlazorTrainingMai22/Pages/Modul5/BootstrapFix.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Components.Forms;
+
+namespace BlazorTrainingMai22.Pages.Modul5
+{
+    public class BootstrapFix:FieldCssClassProvider
+    {
+        public override string GetFieldCssClass(EditContext editContext,
+            in FieldIdentifier fieldIdentifier)
+        {
+            var isinvalid=editContext.GetValidationMessages(fieldIdentifier).Any();
+            return isinvalid ? "is-invalid" : "is-valid";
+        }
+    }
+}
