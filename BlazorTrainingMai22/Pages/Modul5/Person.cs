64c4cd58af5cc6a7a3fb9b22945bbe44b5720fe2
Hannes Preishuber <hannesp@ppedv.de>
2022-05-06 08:42:29 +0200

    Person Model mit Annotation

--- BlazorTrainingMai22/Pages/Modul5/Person.cs
+++ BlazorTrainingMai22/Pages/Modul5/Person.cs
@@ -1,10 +1,17 @@
-﻿namespace BlazorTrainingMai22.Pages.Modul5
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorTrainingMai22.Pages.Modul5
 {
     public class Person
     {
         public int Id { get; set; }
+        [Required(ErrorMessage="bitte füllen")]
+        [StringLength(10, ErrorMessage = "max 10 Zeichen")]
         public string Name { get; set; }
+        [Required(ErrorMessage = "bitte füllen")]
+        [Range(100,190, ErrorMessage = "Größe zwischne 100 und 190cm")]
         public int Gross { get; set; }
+        [Required(ErrorMessage = "bitte füllen")]
         public DateTime GebDat { get; set; }
     }
 }
